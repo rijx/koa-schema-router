a5c748c7e929c2d16e7fb7d05838bdb7438d647d
Mitchell de Rijcke <mdr@rijx.com>
2020-04-03 18:04:19 +0200

    WIP

--- tests/integration/app.js
+++ tests/integration/app.js
@@ -3,10 +3,14 @@ const Koa = require("koa");
 const router = require("../../lib")();
 const routes = require("./routes");
 
-router.define(routes, { prefix: "/api" });
+const api = router.prefix("/api");
+
+for (const route of routes) {
+  api.define(route);
+}
 
 const app = new Koa();
 
-app.use(router);
+app.use(router.routes());
 
 module.exports = app;
